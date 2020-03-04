7a045a00439f79cd90220be8085a16c5f35e77ba
Reddaiah Challa <reddaiah.challa@hexagonsi.com>
2020-03-04 12:47:56 +0530

    Deployment updates

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,7 +1,7 @@
 export default {
     authority: 'https://login.microsoftonline.com/1b16ab3e-b8f6-4fe3-9f3e-2db7fe549f6a',
-    appId: '8c57fcb8-5dd5-4a07-be19-290f1561ddb6',
-    redirectUri: 'https://192.168.116.118:1515',
+    appId: 'af415683-f04d-43f7-9c4d-d1b661963f42',
+    redirectUri: process.env.NODE_ENV === 'production' ? 'https:/192.168.116.118:1515' : 'http://localhost:3000',
     scopes: [
         'user.read',
         'User.ReadBasic.All',
