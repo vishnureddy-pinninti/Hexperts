330584a66d225d6cf28f8547c24230ff5333a4d0
Reddaiah Challa <reddaiah.challa@hexagonsi.com>
2020-04-07 13:45:20 +0530

    redirection url update

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -3,8 +3,8 @@ import uploadImageCallBack from './upload';
 export default {
     authority: 'https://login.microsoftonline.com/1b16ab3e-b8f6-4fe3-9f3e-2db7fe549f6a',
     appId: 'af415683-f04d-43f7-9c4d-d1b661963f42',
-    redirectUri: process.env.NODE_ENV === 'production' ? 'https://hexpert.ingrnet.com' : 'http://localhost:3000',
-    socketUri: process.env.NODE_ENV === 'production' ? 'https://hexpert.ingrnet.com' : 'http://localhost:1515',
+    redirectUri: process.env.NODE_ENV === 'production' ? 'https://hexperts.ingrnet.com' : 'http://localhost:3000',
+    socketUri: process.env.NODE_ENV === 'production' ? 'https://hexperts.ingrnet.com' : 'http://localhost:1515',
     scopes: [
         'user.read',
         'User.ReadBasic.All',
@@ -51,5 +51,5 @@ export default {
     },
 };
 
-// production https://hexpert.ingrnet.com
+// production https://hexperts.ingrnet.com
 // QA https://192.168.116.118:1515
