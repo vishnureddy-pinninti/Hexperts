98084da4e8c26cfa4ee806b0ac2fb06c129d6e23
Reddaiah Challa <reddaiah.challa@hexagonsi.com>
2020-04-08 22:43:43 +0530

    Config update

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -3,8 +3,8 @@ import uploadImageCallBack from './upload';
 export default {
     authority: 'https://login.microsoftonline.com/1b16ab3e-b8f6-4fe3-9f3e-2db7fe549f6a',
     appId: 'af415683-f04d-43f7-9c4d-d1b661963f42',
-    redirectUri: process.env.NODE_ENV === 'production' ? 'https://hexperts.ingrnet.com' : 'http://localhost:3000',
-    socketUri: process.env.NODE_ENV === 'production' ? 'https://hexperts.ingrnet.com' : 'http://localhost:1515',
+    redirectUri: window.location.origin,
+    socketUri: window.location.hostname === 'localhost' ? 'http://localhost:1515': window.location.origin,
     scopes: [
         'user.read',
         'User.ReadBasic.All',
