0190cc130afac937fe959eb7b5d3cb9318d6c488
Karthik Kosigi <karthik.kosigi@hexagon.com>
2020-05-20 20:53:30 +0530

    Added mins to read.

---
+++ client/src/utils/words-to-mins.js
@@ -0,0 +1,12 @@
+const wordsPerMinute = 200; // Average case.
+
+const wordsToMins = (text) => {
+    const textLength = text.split(' ').length; // Split by words
+    if (textLength > 0){
+        return Math.ceil(textLength / wordsPerMinute);
+    }
+
+    return 0;
+};
+
+export default wordsToMins;
